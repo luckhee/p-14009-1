3f1213f36e33d348eb418df859ca5a5262c6704c
luckhee <114234435+luckhee@users.noreply.github.com>
2025-05-22 11:16:03 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -14,6 +14,14 @@ public class Main {
             System.out.print("명령) ");
             String input = sc.nextLine();
             if(input.equals("종료")) break;
+            else if (input.equals("등록")) {
+                System.out.print("명언 :");
+                String input1 = sc.nextLine();
+                System.out.println(input1);
+                System.out.print("작가 :");
+                String input2 = sc.nextLine();
+                System.out.println(input2);
+            }
         }
 
     }
